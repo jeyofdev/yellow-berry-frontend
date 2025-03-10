82d550d0754a256430bf7921e6ed9c4d0efec374
jeyofdev <jgregoire.dev@gmail.com>
2025-03-10 15:08:03 +0100

    style: responsive breadcrumb

--- src/app/shared/components/ui/layout/layout.component.ts
+++ src/app/shared/components/ui/layout/layout.component.ts
@@ -9,11 +9,13 @@ import { Component, InputSignal, OnInit, input } from '@angular/core';
 export class LayoutComponent implements OnInit {
 	public justifyContent: InputSignal<string> = input.required<string>();
 	public alignItems: InputSignal<string> = input<string>('items-start');
+	public flexDirection: InputSignal<string> = input<string>('flex-row');
+	public gap: InputSignal<string> = input<string>('gap-0');
 
 	public styleClass =
 		'flex max-w-screen-sm sm:max-w-screen-md md:max-w-screen-lg lg:max-w-screen-xl xl:max-w-screen-2xl 2xl:max-w-screen-3xl mx-auto px-6 w-full';
 
 	ngOnInit() {
-		this.styleClass += ` ${this.justifyContent()} ${this.alignItems()}`;
+		this.styleClass += ` ${this.justifyContent()} ${this.alignItems()} ${this.flexDirection()} ${this.gap()}`;
 	}
 }
