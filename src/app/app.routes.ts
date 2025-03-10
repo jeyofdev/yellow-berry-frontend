a48f49992fca4f8ac2897c25b21b8869172a8469
jeyofdev <jgregoire.dev@gmail.com>
2025-03-10 13:36:38 +0100

    feat: init routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { LoginPageComponent } from '@features/auth/pages/login-page/login-page.component';
+import { HomePageComponent } from '@features/home/pages/home-page/home-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+	{ path: 'home', component: HomePageComponent },
+	{ path: '', redirectTo: 'home', pathMatch: 'full' },
+	{ path: 'login', component: LoginPageComponent },
+];
