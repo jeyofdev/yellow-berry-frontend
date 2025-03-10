07dd7f51d4dbebdbecc21b252dbb913cd318dabf
Jérémy Grégoire <46073105+jeyofdev@users.noreply.github.com>
2025-03-10 19:34:28 +0100

    refactor - input form login (#12)
    * refactor: create form components text-field and password-field
    
    refactor: create form components text-field and password-field
    
    * refactor: create form components link-form

--- src/app/features/auth/pages/login-page/login-page.component.ts
+++ src/app/features/auth/pages/login-page/login-page.component.ts
@@ -2,11 +2,15 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { FormAuthLogin } from '@models/form/form-auth-login.model';
 import { BreadcrumbComponent } from '@shared/components/ui/breadcrumb/breadcrumb.component';
+import { ButtonFormComponent } from '@shared/components/ui/buttons/button-form/button-form.component';
+import { PasswordFieldComponent } from '@shared/components/ui/form/password-field/password-field.component';
 import { SearchFieldComponent } from '@shared/components/ui/form/search-field/search-field.component';
+import { TextFieldComponent } from '@shared/components/ui/form/text-field/text-field.component';
 import { HeaderNavigationComponent } from '@shared/components/ui/header/header-navigation/header-navigation.component';
 import { HeaderPrimaryNavigationComponent } from '@shared/components/ui/header/header-primary-navigation/header-primary-navigation.component';
 import { HeaderTopbarComponent } from '@shared/components/ui/header/header-topbar/header-topbar.component';
 import { LayoutComponent } from '@shared/components/ui/layout/layout.component';
+import { LinkFormComponent } from '@shared/components/ui/link/link-form/link-form.component';
 import { ButtonModule } from 'primeng/button';
 import { InputTextModule } from 'primeng/inputtext';
 import { PasswordModule } from 'primeng/password';
@@ -25,15 +29,19 @@ import { PasswordModule } from 'primeng/password';
 		ReactiveFormsModule,
 		PasswordModule,
 		ButtonModule,
+		TextFieldComponent,
+		PasswordFieldComponent,
+		ButtonFormComponent,
+		LinkFormComponent,
 	],
 	templateUrl: './login-page.component.html',
 	styleUrl: './login-page.component.scss',
 })
 export class LoginPageComponent implements OnInit {
-	mainForm!: FormGroup<FormAuthLogin>;
+	public mainForm!: FormGroup<FormAuthLogin>;
 
-	emailCtrl!: FormControl<string | null>;
-	passwordCtrl!: FormControl<string | null>;
+	public emailCtrl!: FormControl<string | null>;
+	public passwordCtrl!: FormControl<string | null>;
 
 	constructor(private _formBuilder: FormBuilder) {}
 
