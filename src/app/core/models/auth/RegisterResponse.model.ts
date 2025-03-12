04ce897651f23c8724ceab0c1eaf20f37a76e219
Jérémy Grégoire <46073105+jeyofdev@users.noreply.github.com>
2025-03-12 15:06:56 +0100

    feat - register (#16)
    * feat: register new user
    
    * feat: add inpus password and confirm-password in register form

---
+++ src/app/core/models/auth/RegisterResponse.model.ts
@@ -0,0 +1,6 @@
+export class RegisterResponse {
+	constructor(
+		public message: string,
+		public userId: string,
+	) {}
+}
--- src/app/features/auth/pages/register-page/register-page.component.ts
+++ src/app/features/auth/pages/register-page/register-page.component.ts
@@ -1,13 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { FormAuthLogin } from '@models/form/form-auth-login.model';
+import { RoleEnum } from '@enum/role-enum.enum';
 import { FormAuthRegisterAddress } from '@models/form/form-auth-register-address.model';
 import { FormAuthRegisterContact } from '@models/form/form-auth-register-contact.model';
 import { FormAuthRegisterInfo } from '@models/form/form-auth-register-info.model';
+import { FormAuthRegisterPassword } from '@models/form/form-auth-register-password.model';
 import { FormAuthRegister } from '@models/form/form-auth-register.model';
+import { AuthService } from '@services/auth/auth.service';
 import { BreadcrumbComponent } from '@shared/components/ui/breadcrumb/breadcrumb.component';
 import { ButtonFormComponent } from '@shared/components/ui/buttons/button-form/button-form.component';
 import { MaskFieldComponent } from '@shared/components/ui/form/mask-field/mask-field.component';
+import { PasswordFieldComponent } from '@shared/components/ui/form/password-field/password-field.component';
 import { SearchFieldComponent } from '@shared/components/ui/form/search-field/search-field.component';
 import { TextFieldComponent } from '@shared/components/ui/form/text-field/text-field.component';
 import { HeaderNavigationComponent } from '@shared/components/ui/header/header-navigation/header-navigation.component';
@@ -33,6 +36,7 @@ import { LinkFormComponent } from '@shared/components/ui/link/link-form/link-for
 		LinkFormComponent,
 		TextFieldComponent,
 		MaskFieldComponent,
+		PasswordFieldComponent,
 	],
 	templateUrl: './register-page.component.html',
 	styleUrl: './register-page.component.scss',
@@ -42,6 +46,7 @@ export class RegisterPageComponent {
 	public userInfoGroup!: FormGroup<FormAuthRegisterInfo>;
 	public userContactGroup!: FormGroup<FormAuthRegisterContact>;
 	public userAddressGroup!: FormGroup<FormAuthRegisterAddress>;
+	public userPasswordGroup!: FormGroup<FormAuthRegisterPassword>;
 
 	public firstnameCtrl!: FormControl<string | null>;
 	public lastnameCtrl!: FormControl<string | null>;
@@ -52,8 +57,11 @@ export class RegisterPageComponent {
 	public departmentCtrl!: FormControl<string | null>;
 	public zipCodeCtrl!: FormControl<string | null>;
 	public cityCtrl!: FormControl<string | null>;
+	public passwordCtrl!: FormControl<string | null>;
+	public confirmPasswordCtrl!: FormControl<string | null>;
 
-	constructor(private _formBuilder: FormBuilder) {}
+	private _formBuilder: FormBuilder = inject(FormBuilder);
+	private _authService: AuthService = inject(AuthService);
 
 	ngOnInit(): void {
 		this.initFormControls();
@@ -62,6 +70,20 @@ export class RegisterPageComponent {
 
 	onSubmit(): void {
 		console.log('register datas : ', this.mainForm.value);
+		this._authService
+			.register({
+				email: this.mainForm.value.contact?.email ?? '',
+				password: this.mainForm.value.password?.password ?? '',
+				role: RoleEnum.USER,
+			})
+			.subscribe({
+				next: response => {
+					console.log('Login successful', response);
+				},
+				error: err => {
+					console.error('Error during login', err);
+				},
+			});
 	}
 
 	private initMainForm() {
@@ -69,6 +91,7 @@ export class RegisterPageComponent {
 			info: this.userInfoGroup,
 			contact: this.userContactGroup,
 			address: this.userAddressGroup,
+			password: this.userPasswordGroup,
 		});
 	}
 
@@ -82,6 +105,8 @@ export class RegisterPageComponent {
 		this.departmentCtrl = this._formBuilder.control('');
 		this.cityCtrl = this._formBuilder.control('');
 		this.zipCodeCtrl = this._formBuilder.control('');
+		this.passwordCtrl = this._formBuilder.control('');
+		this.confirmPasswordCtrl = this._formBuilder.control('');
 
 		this.userInfoGroup = this._formBuilder.group({
 			firstname: this.firstnameCtrl,
@@ -99,6 +124,11 @@ export class RegisterPageComponent {
 			department: this.departmentCtrl,
 			zipCode: this.zipCodeCtrl,
 			city: this.cityCtrl,
+		});
+
+		this.userPasswordGroup = this._formBuilder.group({
+			password: this.passwordCtrl,
+			confirmPassword: this.confirmPasswordCtrl,
 		});
 	}
 }
