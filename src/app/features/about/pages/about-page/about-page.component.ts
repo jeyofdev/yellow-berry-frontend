bfd07e6943941e01aba4dbbf0a96b66077de3ee1
jeyofdev <jgregoire.dev@gmail.com>
2025-03-18 14:39:41 +0100

    feat: init section services

--- src/app/features/about/pages/about-page/about-page.component.ts
+++ src/app/features/about/pages/about-page/about-page.component.ts
@@ -3,10 +3,11 @@ import { BreadcrumbComponent } from '@shared/components/ui/breadcrumb/breadcrumb
 import { HeaderComponent } from '@shared/components/ui/header/header/header.component';
 import { LayoutContentComponent } from '@shared/components/ui/layout/layout-content/layout-content.component';
 import { ImageModule } from 'primeng/image';
+import { CardModule } from 'primeng/card';
 
 @Component({
 	selector: 'app-about-page',
-	imports: [HeaderComponent, BreadcrumbComponent, LayoutContentComponent, ImageModule],
+	imports: [HeaderComponent, BreadcrumbComponent, LayoutContentComponent, ImageModule, CardModule],
 	templateUrl: './about-page.component.html',
 	styleUrl: './about-page.component.scss',
 })
