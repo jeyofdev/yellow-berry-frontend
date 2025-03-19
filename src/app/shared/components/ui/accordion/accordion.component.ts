ee0af3ab197b448b0c006f3d3f696eb97c69700e
Jérémy Grégoire <46073105+jeyofdev@users.noreply.github.com>
2025-03-19 16:31:54 +0100

    refactor: create component accordion and use it in page faq (#36)

---
+++ src/app/shared/components/ui/accordion/accordion.component.ts
@@ -0,0 +1,17 @@
+import { Component, InputSignal, input } from '@angular/core';
+import { FaqResponse } from '@models/faq/faq-response.model';
+import { AccordionModule } from 'primeng/accordion';
+
+@Component({
+	selector: 'app-accordion',
+	imports: [AccordionModule],
+	templateUrl: './accordion.component.html',
+	styleUrl: './accordion.component.scss',
+})
+export class AccordionComponent {
+	public items: InputSignal<FaqResponse[]> = input.required<FaqResponse[]>();
+
+	ngOnInit(): void {
+		console.log(this.items());
+	}
+}
