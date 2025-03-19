41672c64ac47db776e485c3d4d2585f482252bf7
jeyofdev <jgregoire.dev@gmail.com>
2025-03-19 16:31:16 +0100

    refactor: create component accordion and use it in page faq

--- src/app/features/faq/pages/faq-page/faq-page.component.ts
+++ src/app/features/faq/pages/faq-page/faq-page.component.ts
@@ -2,16 +2,16 @@ import { ChangeDetectionStrategy, Component, OnInit, WritableSignal, inject, sig
 import { FaqResponse } from '@models/faq/faq-response.model';
 import { SuccessResponse } from '@models/response/success-response.model';
 import { FaqService } from '@services/faq.service';
+import { AccordionComponent } from '@shared/components/ui/accordion/accordion.component';
 import { BreadcrumbComponent } from '@shared/components/ui/breadcrumb/breadcrumb.component';
 import { HeaderComponent } from '@shared/components/ui/header/header/header.component';
 import { LayoutContentComponent } from '@shared/components/ui/layout/layout-content/layout-content.component';
-import { PanelToggleComponent } from '@shared/components/ui/panel-toggle/panel-toggle.component';
 import { ImageModule } from 'primeng/image';
 import { map } from 'rxjs';
 
 @Component({
 	selector: 'app-faq-page',
-	imports: [HeaderComponent, LayoutContentComponent, ImageModule, PanelToggleComponent, BreadcrumbComponent],
+	imports: [HeaderComponent, LayoutContentComponent, ImageModule, BreadcrumbComponent, AccordionComponent],
 	templateUrl: './faq-page.component.html',
 	styleUrl: './faq-page.component.scss',
 	changeDetection: ChangeDetectionStrategy.OnPush,
