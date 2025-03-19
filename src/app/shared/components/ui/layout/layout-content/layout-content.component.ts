3b89da91a313645e7e0aa6ee791db40af352a9aa
jeyofdev <jgregoire.dev@gmail.com>
2025-03-19 18:47:24 +0100

    style: fix page about responsive and margin between sections

--- src/app/shared/components/ui/layout/layout-content/layout-content.component.ts
+++ src/app/shared/components/ui/layout/layout-content/layout-content.component.ts
@@ -12,4 +12,16 @@ export class LayoutContentComponent {
 	public pageTitleSecondary: InputSignal<string> = input<string>('');
 	public pageTitlePrimary: InputSignal<string> = input<string>('');
 	public pageTitleInfo: InputSignal<string> = input<string>('');
+	public containerMarginTop: InputSignal<string> = input<string>('mt-0');
+	public containerMarginBottom: InputSignal<string> = input<string>('mb-0');
+	public childrenContainerMarginTop: InputSignal<string> = input<string>('mt-0');
+	public childrenContainerMarginBottom: InputSignal<string> = input<string>('mb-0');
+
+	public containerStyleClass!: string;
+	public childrenContainerStyleClass!: string;
+
+	ngOnInit(): void {
+		this.containerStyleClass = `flex flex-col justify-center my-0 py-0 ${this.containerMarginTop()} ${this.containerMarginBottom()}`;
+		this.childrenContainerStyleClass = `flex justify-center w-full ${this.childrenContainerMarginTop()} ${this.childrenContainerMarginBottom()}`;
+	}
 }
