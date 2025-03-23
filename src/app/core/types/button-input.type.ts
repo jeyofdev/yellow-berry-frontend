610829856d8e8941fe3b705850bde229516ea33b
jeyofdev <jgregoire.dev@gmail.com>
2025-03-23 17:04:35 +0100

    refactor: create component ui button

--- src/app/core/types/button-input.type.ts
+++ src/app/core/types/button-input.type.ts
@@ -1,1 +1,13 @@
 export type ButtonFormTypeInput = 'submit' | 'reset';
+export type ButtonVariantTypeInput = 'outlined' | 'text' | undefined;
+export type ButtonSeverityTypeInput =
+	| 'success'
+	| 'info'
+	| 'warn'
+	| 'danger'
+	| 'help'
+	| 'primary'
+	| 'secondary'
+	| 'contrast'
+	| null
+	| undefined;
--- src/app/shared/components/ui/card/card-product-details/card-product-details.component.ts
+++ src/app/shared/components/ui/card/card-product-details/card-product-details.component.ts
@@ -2,6 +2,7 @@ import { CommonModule } from '@angular/common';
 import { Component, InputSignal, input } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { ProductDetailsResponse } from '@models/product/product-details-response.model';
+import { ButtonComponent } from '@shared/components/ui/buttons/button/button.component';
 import { RatingComponent } from '@shared/components/ui/rating/rating.component';
 import { ButtonModule } from 'primeng/button';
 import { ImageModule } from 'primeng/image';
@@ -9,7 +10,7 @@ import { InputNumberModule } from 'primeng/inputnumber';
 
 @Component({
 	selector: 'app-card-product-details',
-	imports: [CommonModule, FormsModule, ImageModule, RatingComponent, ButtonModule, InputNumberModule],
+	imports: [CommonModule, FormsModule, ImageModule, RatingComponent, ButtonModule, InputNumberModule, ButtonComponent],
 	templateUrl: './card-product-details.component.html',
 	styleUrl: './card-product-details.component.scss',
 })
