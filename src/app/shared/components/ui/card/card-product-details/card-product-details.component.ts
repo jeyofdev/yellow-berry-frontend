e36df7ddcc890435128644deeeaf5af6ba274979
jeyofdev <jgregoire.dev@gmail.com>
2025-03-23 18:10:00 +0100

    refactor: create component ui number-step-field

--- src/app/shared/components/ui/card/card-product-details/card-product-details.component.ts
+++ src/app/shared/components/ui/card/card-product-details/card-product-details.component.ts
@@ -3,14 +3,22 @@ import { Component, InputSignal, input } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { ProductDetailsResponse } from '@models/product/product-details-response.model';
 import { ButtonComponent } from '@shared/components/ui/buttons/button/button.component';
+import { NumberStepFieldComponent } from '@shared/components/ui/form/number-step-field/number-step-field.component';
 import { RatingComponent } from '@shared/components/ui/rating/rating.component';
 import { ButtonModule } from 'primeng/button';
 import { ImageModule } from 'primeng/image';
-import { InputNumberModule } from 'primeng/inputnumber';
 
 @Component({
 	selector: 'app-card-product-details',
-	imports: [CommonModule, FormsModule, ImageModule, RatingComponent, ButtonModule, InputNumberModule, ButtonComponent],
+	imports: [
+		CommonModule,
+		FormsModule,
+		ImageModule,
+		RatingComponent,
+		ButtonModule,
+		ButtonComponent,
+		NumberStepFieldComponent,
+	],
 	templateUrl: './card-product-details.component.html',
 	styleUrl: './card-product-details.component.scss',
 })
