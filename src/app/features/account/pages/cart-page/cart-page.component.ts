89d9afb6c4f6a0d9b340c97a326f149ff81b65a0
jeyofdev <jgregoire.dev@gmail.com>
2025-04-09 18:37:49 +0200

    feat: update total price when product quanty change

--- src/app/features/account/pages/cart-page/cart-page.component.ts
+++ src/app/features/account/pages/cart-page/cart-page.component.ts
@@ -3,10 +3,11 @@ import { BreadcrumbComponent } from '@shared/components/ui/breadcrumb/breadcrumb
 import { HeaderComponent } from '@shared/components/ui/header/header/header.component';
 import { LayoutContentComponent } from '@shared/components/ui/layout/layout-content/layout-content.component';
 import { ListCartProductComponent } from '@shared/components/ui/list/list-cart-product/list-cart-product.component';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
 	selector: 'app-cart-page',
-	imports: [HeaderComponent, BreadcrumbComponent, LayoutContentComponent, ListCartProductComponent],
+	imports: [HeaderComponent, BreadcrumbComponent, LayoutContentComponent, ListCartProductComponent, ButtonModule],
 	templateUrl: './cart-page.component.html',
 	styleUrl: './cart-page.component.scss',
 })
